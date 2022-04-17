a03e486574c80fc7c132fbb1a669fc77f557c6c3
Sontsu <72311577+Sontsu@users.noreply.github.com>
2022-04-17 20:40:55 +0900

    test

---
+++ sampleAPI.py
@@ -0,0 +1,72 @@
+import pandas as pd
+import spotipy
+from spotipy.oauth2 import SpotifyClientCredentials
+import time
+
+# spotify developerから取得したclient_idとclient_secretを入力
+client_id = 'e76c88811ef9456eba51a877c24144b6'
+client_secret = 'ccfa5e055e624ea194432bc19647b0cd'
+
+client_credentials_manager = SpotifyClientCredentials(client_id, client_secret)
+sp = spotipy.Spotify(client_credentials_manager=client_credentials_manager)
+
+def getTrackIDs(playlist_ids):
+    track_ids = []
+  
+    for playlist_id in playlist_ids:
+        playlist = sp.playlist(playlist_id)
+        while playlist['tracks']['next']:
+            for item in playlist['tracks']['items']:
+                track = item['track']
+                if not track['id'] in track_ids:
+                    track_ids.append(track['id'])
+            playlist['tracks'] = sp.next(playlist['tracks'])
+        else:
+            for item in playlist['tracks']['items']:
+                track = item['track']
+                if not track['id'] in track_ids:
+                    track_ids.append(track['id'])
+    return track_ids
+
+playlist_ids = ["37i9dQZEVXbKqiTGXuCOsB"]  # SpotifyのプレイリストのIDを入力
+track_ids = getTrackIDs(playlist_ids)
+print(len(track_ids))
+print(track_ids)
+
+def getTrackFeatures(id):
+    meta = sp.track(id)
+    features = sp.audio_features(id)
+
+    name = meta['name']
+    album = meta['album']['name']
+    artist = meta['album']['artists'][0]['name']
+    release_date = meta['album']['release_date']
+    length = meta['duration_ms']
+    popularity = meta['popularity']
+    key = features[0]['key']
+    mode = features[0]['mode']
+    danceability = features[0]['danceability']
+    acousticness = features[0]['acousticness']
+    energy = features[0]['energy']
+    instrumentalness = features[0]['instrumentalness']
+    liveness = features[0]['liveness']
+    loudness = features[0]['loudness']
+    speechiness = features[0]['speechiness']
+    tempo = features[0]['tempo']
+    time_signature = features[0]['time_signature']
+    valence = features[0]['valence']
+
+    track = [name, album, artist, release_date, length, popularity, key, mode, danceability, acousticness, energy, instrumentalness, liveness, loudness, speechiness, tempo, time_signature, valence]
+    return track
+
+tracks = []
+
+for track_id in track_ids:
+  time.sleep(0.5)
+  track = getTrackFeatures(track_id)
+  tracks.append(track)
+
+df = pd.DataFrame(tracks, columns = ['name', 'album', 'artist', 'release_date', 'length', 'popularity', 'key', 'mode', 'danceability', 'acousticness', 'energy', 'instrumentalness', 'liveness', 'loudness', 'speechiness', 'tempo', 'time_signature', 'valence'])
+df.head()
+
+df.to_csv('spotify_music_data.csv', sep = ',')
